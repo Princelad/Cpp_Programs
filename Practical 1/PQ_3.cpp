cd5aac1535f2189c35fc46c9689c2cce87af7017
Princelad <ladprince1112@gmail.com>
2024-01-06 13:30:56 +0530

    1.0

---
+++ Practical 1/PQ_3.cpp
@@ -0,0 +1,10 @@
+/*This program is created by 23CS037 Prince Lad*/
+
+/*
+Create an advanced C++ program for tracking daily expenses. The program should allow the user to
+input details of expenses. It should display a running total of expenses.
+*/
+
+#include <iostream>
+
+using namespace std;
