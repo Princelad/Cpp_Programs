7b7617d31cf3ee5b8d441baa29c43b1d0f0bf891
Princelad <ladprince1112@gmail.com>
2024-04-15 19:10:53 +0530

    1.0

--- Practical 15/main.cpp
+++ Practical 15/main.cpp
@@ -39,6 +39,12 @@ public:
 
     void output() {
 
+        cout << "=---------------------------------------------------------------------------------------------------------------------=" << endl;
+        
+        cout << product_id << " : " << product_name << " : " << product_manufacturer << " : " << product_price << " : " << dial_size << endl;
+
+        cout << "=---------------------------------------------------------------------------------------------------------------------=" << endl;
+
     }
 
 };
@@ -54,6 +60,12 @@ public:
     }
 
     void output() {
+        
+        cout << "=---------------------------------------------------------------------------------------------------------------------=" << endl;
+        
+        cout << product_id << " : " << product_name << " : " << product_manufacturer << " : " << product_price << " : " << width << " : " << height << endl;
+
+        cout << "=---------------------------------------------------------------------------------------------------------------------=" << endl;
 
     }
 
@@ -61,6 +73,95 @@ public:
 
 int main() {
 
+    int Productid, option;
+    string Productname, Productmanufacturer;
+    float Productprice, Dialsize, Width, Height;
 
+    do {
+        cout << "=------------------------------------------------------= Main Menu =-------------------------------------------------=" << endl;
+
+        cout << setw(5) << left << "[1]" << "Smart watch." << endl;
+        cout << setw(5) << left << "[2]" << "Bed sheet." << endl;
+
+        cout << "=--------------------------------------------------------------------------------------------------------------------=" << endl;
+
+        cout << setw(25) << "Choose the option" << ": ";
+        cin >> option;
+        cout << option;
+
+        cout << "=--------------------------------------------------------------------------------------------------------------------=" << endl;
+
+        if (option == 1 || option == 2) {
+           break;
+        }
+        else {
+            cout << "=---------------------------------------------------------------------------------------------------------------------=" << endl;
+
+            cout << setw(51) << "" << "Enter proper input." << endl;
+
+            cout << "=---------------------------------------------------------------------------------------------------------------------=" << endl;
+        }
+
+    } while (true);
+
+
+    switch(option) {
+        case 1: {
+
+            cout << "=------------------------------------------------= Enter Smartwatch Data =-------------------------------------------=" << endl;
+
+            cout << setw(35) << left << "Enter product I.D." << ": ";
+            cin >> Productid;
+            cout << setw(35) << left << "Enter product name" << ": ";
+            cin >> Productname;
+            cout << setw(35) << left << "Enter product manufacturer" << ": ";
+            cin >> Productmanufacturer;
+            cout << setw(35) << left << "Enter product price" << ": ";
+            cin >> Productprice;
+            cout << setw(35) << left << "Enter product dial size" << ": ";
+            cin >> Dialsize;
+
+            smartwatch *temp;
+
+            temp = new smartwatch(Productid, Productname, Productmanufacturer, Productprice, Dialsize);
+
+            temp -> output();
+
+            delete temp;
+
+            break;            
+
+        }
+        case 2: {
+
+            cout << "=-------------------------------------------------= Enter Bedsheet Data =--------------------------------------------=" << endl;
+
+            cout << setw(35) << left << "Enter product I.D." << ": ";
+            cin >> Productid;
+            cout << setw(35) << left << "Enter product name" << ": ";
+            cin >> Productname;
+            cout << setw(35) << left << "Enter product manufacturer" << ": ";
+            cin >> Productmanufacturer;
+            cout << setw(35) << left << "Enter product price" << ": ";
+            cin >> Productprice;
+            cout << setw(35) << left << "Enter product width" << ": ";
+            cin >> Width;
+            cout << setw(35) << left << "Enter product height" << ": ";
+            cin >> Height;
+
+            bedsheet *temp;
+
+            temp = new bedsheet(Productid, Productname, Productmanufacturer, Productprice, Width, Height);
+
+            temp -> output();
+
+            delete temp;
+
+            break;            
+
+        }
+    }
+
+    cout << "\n\n23CS037 Prince" << endl;
     return 0;
 }
